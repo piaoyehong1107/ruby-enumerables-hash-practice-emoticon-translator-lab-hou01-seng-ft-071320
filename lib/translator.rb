4992d4fd4d818fd2c27572269896e664461061d7
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-02 04:16:04 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,46 @@
 # require modules here
-
-def load_library
-  # code goes here
+require "pry"
+def load_library(file_path)
+  emoticons_newhash = { }
+  require 'yaml'
+  emoticons_hash = YAML.load(File.read(file_path))
+  emoticons_hash.each { |key,value|
+  emoticons_newhash.store(key,{:english=>value[0],:japanese=>value[1]})
+  }
+emoticons_newhash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+ emoticons_newhash = load_library(file_path)
+emojis = [ ]
+ emoticons_newhash.each {|language, emoji|
+  emoji.each {|inner_key,inner_value|
+    emojis.push(inner_value)
+  }
+ }
+  if (emojis.include?(emoticon))
+  emojiss = emoticons_newhash.select{|key, hash| hash[:english] == emoticon }
+  meaning = emojiss.keys[0]
+  emojiss[meaning][:japanese]
+  else
+  p "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(file_path, emoticon)
+ emoticons_newhash = load_library(file_path)
+  emojis = [ ]
+  emoticons_newhash.each {|language, emoji|
+  emoji.each {|inner_key,inner_value|
+    emojis.push(inner_value)
+  }
+ }
+  if (emojis.include?(emoticon))
+  emojiss = emoticons_newhash.select{|key, hash| hash[:japanese] == emoticon }
+  p emojiss.keys[0]
+  else
+  p "Sorry, that emoticon was not found"
+  end
 end
+#binding.pry
